482326305a9d5c1281d7ee79d366d47f0fbd064c
Emam Uddin <emamskingdom@gmail.com>
2025-02-12 22:31:49 +0600

    added home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,13 +5,13 @@ import { authContext } from '../Root/Root';
 
 const Home = () => {
    const contextValue = useContext(authContext)
-   const {handleGoogleLogin,
-    handleGithubLogin}= contextValue
+   const {handleGoogleLogin,handleGithubLogin,handleLogOut}= contextValue
    console.log(contextValue)
     return (
         <div>
             <button className='btn' onClick={handleGoogleLogin}>Google Login </button>
             <button className='btn' onClick={handleGithubLogin}>Github Login </button>
+            <button onClick={handleLogOut} className='btn'>Logout</button>
         </div>
     );
 };
